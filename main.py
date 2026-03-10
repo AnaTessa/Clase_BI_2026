55e0bf5a3426c6054c9b75365654a72f08f42c27
AnaTessa <125513815+AnaTessa@users.noreply.github.com>
2026-03-09 20:34:58 -0600

    Update logo image in main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def show_header(text_title: str):
     col1, col2 = st.columns([1, 6])
     
     with col1:
-        st.image("imagenes/UPlogo.jpg", width=200)
+        st.image("A95628AB-88C8-4FAA-A86D-A877C52152F0.jpg", width=200)
         
     with col2:
         st.title(text_title)
